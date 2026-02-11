9f988e972ae7355bae95de6c79ed0b16d41bc16c
Wojciech Galant <wojtekgalant7@gmail.com>
2026-02-11 21:35:15 +0100

    refactor!: rewrite application from scratch using Clean Architecture

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -200,16 +200,18 @@
 
 <section id="strona-glowna">
     <div class="content">
-        <img class="tlo_strony1" src="https://images.pexels.com/photos/1639561/pexels-photo-1639561.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+        <img class="tlo_strony1"
+             src="https://images.pexels.com/photos/1639561/pexels-photo-1639561.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
         <div class="text-on-image">Witaj w naszej <span class="restauracja">Restauracji</span></div>
-        <div class="text-on-image2">Jesteśmy z wami 10 lat </div>
+        <div class="text-on-image2">Jesteśmy z wami 10 lat</div>
     </div>
 </section>
 
 
 <section id="info">
     <h2>Info</h2>
-    <img class="tlo_strony2" src="https://images.pexels.com/photos/1126728/pexels-photo-1126728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+    <img class="tlo_strony2"
+         src="https://images.pexels.com/photos/1126728/pexels-photo-1126728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
 
 </section>
 
@@ -217,78 +219,93 @@
     <h2>Menu</h2>
     <div class="menucontent">
         <div class="carousel-container">
-            <img class="tlo_strony3" src="https://images.pexels.com/photos/313700/pexels-photo-313700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+            <img class="tlo_strony3"
+                 src="https://images.pexels.com/photos/313700/pexels-photo-313700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
             <div class="carousel_slide" data-bs-ride="carousel">
 
                 <div class="carousel-indicators">
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="4"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="5"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="6"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="7"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="8"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="9"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="4"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="5"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="6"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="7"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="8"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="9"></button>
+                </div>
+
+
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0004.jpg" alt="Los Angeles"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0005.jpg" alt="Chicago"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0006.jpg" alt="New York"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0007.jpg" alt="New York"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0008.jpg" alt="New York"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0009.jpg" alt="New York"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0010.jpg" alt="New York"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0011.jpg" alt="New York"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0012.jpg" alt="New York"
+                             class="d-block w-100">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0013.jpg" alt="New York"
+                             class="d-block w-100">
+                    </div>
+                </div>
+
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </button>
+            </div>
         </div>
-
-
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0004.jpg" alt="Los Angeles" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0005.jpg" alt="Chicago" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0006.jpg" alt="New York" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0007.jpg" alt="New York" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0008.jpg" alt="New York" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0009.jpg" alt="New York" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0010.jpg" alt="New York" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0011.jpg" alt="New York" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0012.jpg" alt="New York" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="https://www.browarikuchnia.pl/wp-content/uploads/2023/03/0013.jpg" alt="New York" class="d-block w-100">
-            </div>
-        </div>
-
-
-        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon"></span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </button>
-    </div>
     </div>
 </section>
 
 <section id="galeria">
-    <img class="tlo_strony4" src="https://images.pexels.com/photos/735869/pexels-photo-735869.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+    <img class="tlo_strony4"
+         src="https://images.pexels.com/photos/735869/pexels-photo-735869.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
 </section>
 
 <section id="kucharze">
-    <img class="tlo_strony5" src="https://images.pexels.com/photos/2544829/pexels-photo-2544829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+    <img class="tlo_strony5"
+         src="https://images.pexels.com/photos/2544829/pexels-photo-2544829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
 
 </section>
 
 <section id="kontakt">
-    <img class="tlo_strony6" src="https://images.pexels.com/photos/1416530/pexels-photo-1416530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+    <img class="tlo_strony6"
+         src="https://images.pexels.com/photos/1416530/pexels-photo-1416530.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
 
 </section>
 </body>
